2ebd4cbecc25a54c15c1808f288ea781cdff2c5b
Ruthmaelle <ruthmaelle06@gmail.com>
2023-10-27 08:38:18 -0400

    innit

--- functions.php
+++ functions.php
@@ -1,14 +1,76 @@
 <?php
+
+function showData($title, $data) {
+    echo "<br><br><h2>".$title."</h2>";
+    var_dump($data);
+    
+}
+
 function selectUserByIndex($id) {
     //recuperer une ligne dans user
     global $conn;
-    $result = mysqli_query($conn, "SELECT * FROM user WHERE id = " . $id);
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE id = 1" . $id);
 
     //avec fecth row : tableau indexe
-    $date = mysqli_fetch_row($result);
+    $data = mysqli_fetch_row($result);
     
-    return $date;
+    return $data;
+}
+ 
+function selectUserByAssoc($id) {
+    global $conn;
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE id = " . $id);
+    
+    //avec fecth row : tableau indexe
+    $data = mysqli_fetch_assoc($result);
+
+    return $data;
 }
 
-function selectUserByAssoc()
+
+
+function getAllUserAssoc() {
+    $result = mysqli_query($conn, "SELECT user_name, email, id FROM user");
+
+    $data = [];
+    $i = 0;
+    while ($rangeeData = mysqli_fetch_assoc($result)) {
+    $data[$i] = $rangeeData;
+    $i++;
+    };
+
+    /*$imax = mysqli_num_rows($result5);
+
+    for ($i = 0; $i < $imax; $i++ ) {
+        $rangeeData = mysqli_fetch_assoc($result5);
+        $data5[$i] = $rangeeData;
+        
+    }*/
+    return $data;
+}
+/*function createUser($data){
+    global $conn
+    $query = "INSERT INTO `user` (`id`, `user_name`, `email`, `pwd`) VALUES (NULL, 'Mal', 'mal@mal.ca', '')";;
+    $result = mysqli_query($conn, $query);
+}
+*/
+
+function createUser($data){
+    global $conn;
+    $query = 'INSERT INTO user VALUES (NULL, ?, ?, ?)';
+
+}
+
+function updateUser($data) {
+    global $conn ;
+    $query = "SELECT * FROM user WHERE user_name = " . $user_name;
+    $result = mysqli_query($conn, $query);
+}
+
+function deleteUser($data) {
+    global $conn;
+    $query = "SELECT * FROM user WHERE user_name = " " " . $user_name;
+    $del = delete($query);
+    $result = mysqli_query($conn, $query);
+}
 ?>
--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <?php
-$server = 'localhost';
+require_once('./functions.php'); //pour appeler un autre fichier
+
+$server = 'localhost'; //4-7 pour appeler la base de donnee
 $userName = 'root';
 $pwd = '';
 $db = 'ecom1';
 
-$conn = mysqli_connect($server, $userName, $pwd, $db);
+$conn = mysqli_connect($server, $userName, $pwd, $db);  //pour se connecter avec la base de donnee
 if ($conn) {
     echo "connected to the $db database succesfully";
-    global $conn;
+    global $conn; 
 } else {
     echo "Error: Not connected to the $db database";
 }
 
 //recuperer un ligne dans le tableau dans user 
-$result1= mysqli_query($conn, "SELECT * from user where id = 2");
+//$result1= mysqli_query($conn, "SELECT * from user where id = 2");
 
 //PREMIER FETCH
 //avec fetch row : permet de recuperer un donne dans un tableau indexee
-$data1 = mysqli_fetch_row($result1);
-
+//$data1 = mysqli_fetch_row($result1);
+$data1 = selectUserByIndex();
+showData("premier fetch", $data1);
 
 echo "<br>";
 echo "premier fetch";
@@ -36,7 +39,7 @@ $result2 = mysqli_query($conn, "SELECT * from user where id = 1");
 $data2 = mysqli_fetch_assoc($result2);
 //avec fetch assoc : permet de recuperer un donne dans un tableau associatif 
 
-echo "<br>";
+/*echo "<br>"; //on utilise des fonctions de preference
 echo "second fetch";
 echo "<br>";
 echo "<br>";
@@ -44,11 +47,14 @@ var_dump($result2);
 echo "<br>";
 echo "<br>";
 var_dump($data2);
+*/
 
+$data2 = selectUserByIndex(2);
+showData("second fetch", $data2);
 
 //TROISIEME FETCH
 //recuperer une seule ligne mais en choisissant l'ordre des colonnes 
-$result3 = mysqli_query($conn, "SELECT user_name, email, id from user where id = 1");
+/*$result3 = mysqli_query($conn, "SELECT user_name, email, id from user where id = 1");
 $data3 = mysqli_fetch_assoc($result3);
 
 echo "<br>";
@@ -59,12 +65,14 @@ var_dump($result3);
 echo "<br>";
 echo "<br>";
 var_dump($data3);
+*/
+
 
 //QUATRIEME FETCH
 //recuperer toutes les lignes de la table user
 //avec fetch assoc
 
-$result4 = mysqli_query($conn, "SELECT user_name, email, id FROM user");
+/*$result4 = mysqli_query($conn, "SELECT user_name, email, id FROM user");
 
 $data4 = [];
 $i = 0;
@@ -88,6 +96,22 @@ var_dump($result4);
 echo "<br>";
 echo "<br>";
 var_dump($data4);
+*/
+
+$data4 = getAllUserAssoc();
+showData('Quatrieme fetch', $data4);
+echo "<ul>";
+
+/*foreach($data4 as $key => $value){
+    ?> 
+<li>L'utilisateur se nomme : <?php echo $value['user_name'] ?>.
+Son ID est : <?php echo $value['id']?>.
+Son Email est : <?php echo $value['email']?> </li>
+<?php>
+}*/
+
+echo"<ul>";
+
 
 //avec la boucle for 
 $result5 = mysqli_query($conn, "SELECT user_name, email, id from user");
@@ -142,4 +166,15 @@ var_dump($result6);
 echo "<br>";
 echo "<br>";
 var_dump($data6);
+
+
+$data7 = [
+    'user_name' => 'William',
+    'email' => 'bill@bill.ca',
+    'pwd' => ''
+];
+
+$newUser = createUser($data7);
+
+$users = getAllUserAssoc();
 ?>
